5261e22c0b4ccdb7739b9cd392933e9d496371b7
rcdavis4 <rcdavis4@gmail.com>
2015-04-08 16:39:50 -0700

    start building objects

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,5 +1,54 @@
 $(document).ready(function() {
 
+  /*--- GLOBALS ---*/
+  var questionNum;
 
 
-}); // end of document.ready
+  /*--- QUIZ OBJECTS ---*/
+  var quiz = [
+    {
+      question: questionNum + "That chick Julie, she's truly dazzling...Yep, but she's not one of ours.",
+      choices: ["Valley Girl", "2", "3", "4", "5"],
+      answer: "Valley Girl";
+    },
+
+    {
+      question: questionNum + "This is pure snow. It's everywhere! Do you have any idea what the street value of this mountain is?",
+      choices: ["Better Off Dead", "2", "3", "4", "5"],
+      answer: "Better Off Dead";
+    },
+
+    {
+      question: questionNum + " add questions here ",
+      choices: ["1", "2", "3", "4", "5"],
+      answer: "answers";
+    },
+
+    {
+      question: questionNum + " add questions here ",
+      choices: ["1", "2", "3", "4", "5"],
+      answer: "answers";
+    },
+
+    {
+      question: questionNum + " add questions here ",
+      choices: ["1", "2", "3", "4", "5"],
+      answer: "answers";
+    },
+  ]
+
+  /*--- FUNCTIONS ---*/
+
+
+
+
+  /*--- EVENTS ---*/
+  $('.js-question').click(function() {
+
+  });
+
+
+/* increment questionNum each time calling new question object */
+
+
+}); // end document.ready
