901e366ff965c5430c6274cf351fb2bcc45733f6
rcdavis4 <rcdavis4@gmail.com>
2015-04-10 14:29:33 -0700

    adding content dynamically

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,54 +1,89 @@
+"use strict";
+
 $(document).ready(function() {
 
   /*--- GLOBALS ---*/
-  var questionNum;
+  var questionIndex = 0;
+  var correctMsg = "Correct!";
+  var incorrectMsg = "Sorry, that is just wrong.";
 
 
   /*--- QUIZ OBJECTS ---*/
-  var quiz = [
+  var Quiz = [
     {
-      question: questionNum + "That chick Julie, she's truly dazzling...Yep, but she's not one of ours.",
+      question: "'That chick Julie, she's truly dazzling...Yep, but she's not one of ours.'",
       choices: ["Valley Girl", "2", "3", "4", "5"],
-      answer: "Valley Girl";
+      answer: "Valley Girl",
+      correct: false
     },
 
     {
-      question: questionNum + "This is pure snow. It's everywhere! Do you have any idea what the street value of this mountain is?",
+      question: "'This is pure snow. It's everywhere! Do you have any idea what the street value of this mountain is?'",
       choices: ["Better Off Dead", "2", "3", "4", "5"],
-      answer: "Better Off Dead";
+      answer: "Better Off Dead",
+      correct: false
     },
 
     {
-      question: questionNum + " add questions here ",
-      choices: ["1", "2", "3", "4", "5"],
-      answer: "answers";
+      question: "'I got a question. If you guys know so much about women, how come you're here at like the Gas 'n' Sip on a Saturday night completely alone drinking beers with no women anywhere?'...'By choice!'",
+      choices: ["Say Anything", "Fast Times At Ridgemont High", "Revenge Of The Nerds", "4", "5"],
+      answer: "Say Anything",
+      correct: false
     },
 
     {
-      question: questionNum + " add questions here ",
-      choices: ["1", "2", "3", "4", "5"],
-      answer: "answers";
+      question: "'Sweep the leg.'",
+      choices: ["Karate Kid", "2", "3", "4", "5"],
+      answer: "Karate Kid",
+      correct: false
     },
 
     {
-      question: questionNum + " add questions here ",
-      choices: ["1", "2", "3", "4", "5"],
-      answer: "answers";
-    },
+      question: "'What I wouldn't give to go ass sliding with you right now.'",
+      choices: ["Rad", "2", "3", "4", "5"],
+      answer: "Rad",
+      correct: false
+    }
   ]
 
   /*--- FUNCTIONS ---*/
 
+  // load questions
+  function addQuestion() {
+    $('.js-question').html(Quiz[questionIndex].question);
+
+    questionIndex++;
+  }
+
+  function addChoices() {
+//    var choiceIndex = 0;
+
+    $('.js-choices').append("<label for='radio'>gggg</label>");
+
+  }
+
+// for (indices in question && indices in choices)
+// <label for="radio">choices</label>
+// $('input:selected').siblings('label')
+
+  /*--- FUNCTION CALLS ---*/
+  addQuestion();
+  addChoices();
 
 
+console.log(Quiz[0].question);
+console.log(Quiz[0].choices[0]);
 
   /*--- EVENTS ---*/
-  $('.js-question').click(function() {
+//  $('.js-question').click(function() {
+    // load new question
+    // load new choices
+    // compare answer with selected choice
+    // display message
+//  });
 
-  });
 
 
-/* increment questionNum each time calling new question object */
 
 
 }); // end document.ready
